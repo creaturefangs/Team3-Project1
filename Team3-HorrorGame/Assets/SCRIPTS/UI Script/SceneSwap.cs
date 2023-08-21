c99943a574f182d6a5662eec12f0a810d81e0ec1
Fuss360 <142345812+Fuss360@users.noreply.github.com>
2023-08-21 15:56:38 -0400

    Credit Scene Button Fix

---
+++ Team3-HorrorGame/Assets/SCRIPTS/UI Script/SceneSwap.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwap : MonoBehaviour
+{
+
+    private void OnGUI()
+    {
+        int xCenter = (Screen.width / 2);
+        int yCenter = (Screen.height / 2);
+        int width = 400;
+        int height = 120;
+
+        GUIStyle fontSize = new GUIStyle(GUI.skin.GetStyle("button"));
+        fontSize.fontSize = 32;
+
+        Scene scene = SceneManager.GetActiveScene();
+
+        if (scene.name == "scene1")
+        {
+            // Show a button to allow scene2 to be switched to.
+            if (GUI.Button(new Rect(xCenter - width / 2, yCenter - height / 2, width, height), "Load second scene", fontSize))
+            {
+                SceneManager.LoadScene("scene2");
+            }
+        }
+        else
+        {
+            // Show a button to allow scene1 to be returned to.
+            if (GUI.Button(new Rect(xCenter - width / 2, yCenter - height / 2, width, height), "Return to first scene", fontSize))
+            {
+                SceneManager.LoadScene("scene1");
+            }
+        }
+    }
+
+}
