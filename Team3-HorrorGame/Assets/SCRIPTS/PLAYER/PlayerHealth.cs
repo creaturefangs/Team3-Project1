355ad576bb49694bef0f2dc985b464d8779e84ff
creaturefangs <126419720+creaturefangs@users.noreply.github.com>
2023-08-28 16:19:12 -0400

    Pushes

--- Team3-HorrorGame/Assets/SCRIPTS/PLAYER/PlayerHealth.cs
+++ Team3-HorrorGame/Assets/SCRIPTS/PLAYER/PlayerHealth.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+
+    public GameObject pickupHealthkit;
+    public GameObject pickupPills;
     // Start is called before the first frame update
     void Start()
     {
