a818ff635a60dfceee40841ca5e0f25b66ef89ab
creaturefangs <126419720+creaturefangs@users.noreply.github.com>
2023-08-30 12:33:08 -0400

    enemypush

--- Team3-HorrorGame/Assets/SCRIPTS/ENEMIES/BasicEnemyAI.cs
+++ Team3-HorrorGame/Assets/SCRIPTS/ENEMIES/BasicEnemyAI.cs
@@ -10,16 +10,66 @@ public class BasicEnemyAI : MonoBehaviour
     public AudioSource goreSFX;
     public GameObject playerDmgUI;
 
-    // Start is called before the first frame update
-    void Start()
+    public Transform[] waypoints;
+    public Transform player;
+    public float detectionRange = 10f;
+    public float patrolSpeed = 2f;
+    public float chaseSpeed = 4f;
+
+    private Transform currentWaypoint;
+    private int waypointIndex = 0;
+    private Animator animator;
+
+    private enum AIState { Patrol, Chase }
+    private AIState currentState = AIState.Patrol;
+
+    private void Start()
     {
-        
+        currentWaypoint = waypoints[waypointIndex];
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        switch (currentState)
+        {
+            case AIState.Patrol:
+                Patrol();
+                break;
+            case AIState.Chase:
+                Chase();
+                break;
+        }
+    }
+
+    private void Patrol()
+    {
+        float step = patrolSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, step);
+
+        if (Vector3.Distance(transform.position, currentWaypoint.position) < 0.1f)
+        {
+            waypointIndex = (waypointIndex + 1) % waypoints.Length;
+            currentWaypoint = waypoints[waypointIndex];
+        }
+
+        // Check if player is within detection range
+        if (Vector3.Distance(transform.position, player.position) < detectionRange)
+        {
+            currentState = AIState.Chase;
+        }
+    }
+
+    private void Chase()
+    {
+        float step = chaseSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, player.position, step);
+
+        // Check if player is out of detection range
+        if (Vector3.Distance(transform.position, player.position) > detectionRange)
+        {
+            currentState = AIState.Patrol;
+        }
     }
 
     void OnTriggerEnter(Collider other)
