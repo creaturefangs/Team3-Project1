1ecc29d56e3ecf9e217612e5d6d1b9aced24fb5d
creaturefangs <126419720+creaturefangs@users.noreply.github.com>
2023-09-11 18:44:42 -0400

    levelonepush

--- Team3-HorrorGame/Assets/Resources/SCRIPTS/PLAYER/TaskListManager.cs
+++ Team3-HorrorGame/Assets/Resources/SCRIPTS/PLAYER/TaskListManager.cs
@@ -19,7 +19,7 @@ public class TaskListManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T))
+        if (Input.GetKeyDown(KeyCode.Tab))
         {
             if (pauseManager.GameIsPaused == true)
             {
